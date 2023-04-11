be42ffdfdd00af0023f279cb7168a007c3b079ff
Jitendra-patel1 <pateljitendra6061@gmail.com>
2023-04-11 20:57:09 +0530

    assisgnment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+
+        RWOnly obj = new RWOnly();
+        // obj.name="Jitendra";
+        // 'name' has private access in 'com.driver.RWOnly'
+        obj.setName("Jitendra");
+        String ans=obj.getName();
+        System.out.println(ans);
+
+    }
 }
